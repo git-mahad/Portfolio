dca784bcc037b8f0581834b8be57b485833c70e9
Muhammad Mahad <mahad.dev3@gmail.com>
2025-02-16 00:12:40 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <p className="bg-red-500 text-white p-4">If this is red, Tailwind is working!</p>
+    </div>
+  )
+}
+
+export default App
