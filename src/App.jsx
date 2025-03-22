0330e3bcd268c9faa3206ee619af1198496b9c48
Muhammad Mahad <mahad.dev3@gmail.com>
2025-03-22 13:49:07 +0500

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+function Home() { /* delet this function*/
+  return <h1>Welcome to the Home Page</h1>;
+}
+
+function NotFound() {  /* delet this function*/
+  return <h1>404 - Page Not Found</h1>;
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',  // customise according to your project
+    element: <Home />,
+  },
+  {
+    path: '*',  // customise according to your project
+    element: <NotFound />,
+  },
+]);
 
 function App() {
   return (
     <div className="min-h-screen">
+      <RouterProvider router={router} />
       <p className="bg-red-500 text-white p-4">If this is red, Tailwind is working!</p>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
-/** @type {import('tailwindcss').Config} */
-
-import daisyui from "daisyui"
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [
-    require('daisyui'),
-  ],
+  plugins: [],
 }
